612c3a1db70165e0364e16234995ce13f8561f1b
Roberto Tatasciore <tatasciorecont@gmail.com>
2020-07-25 19:38:04 +0200

    Add loading support to AppButton

--- src/components/AppButton/AppButton.tsx
+++ src/components/AppButton/AppButton.tsx
@@ -6,22 +6,31 @@ import {
   StyleProp,
   ViewStyle,
   TextStyle,
+  ActivityIndicator,
 } from 'react-native';
 import styles from './styles';
 
 interface AppButtonProps {
   text: string;
+  loading?: boolean;
   onPress: () => void;
   style?: StyleProp<ViewStyle>;
   textStyle?: StyleProp<TextStyle>;
 }
 
 export const AppButton = (props: AppButtonProps) => {
-  const {text, onPress, style, textStyle} = props;
+  const {text, loading, onPress, style, textStyle} = props;
   return (
     <TouchableOpacity onPress={onPress}>
       <View style={[styles.container, style]}>
-        <Text style={[styles.text, textStyle]}>{text}</Text>
+        <Text style={[styles.text, {marginRight: 8}, textStyle]}>{text}</Text>
+        {loading && (
+          <ActivityIndicator
+            color="#fff"
+            size="small"
+            style={{alignSelf: 'center'}}
+          />
+        )}
       </View>
     </TouchableOpacity>
   );
--- src/components/AppButton/styles.ts
+++ src/components/AppButton/styles.ts
@@ -2,11 +2,13 @@ import {StyleSheet} from 'react-native';
 
 export default StyleSheet.create({
   container: {
+    flexDirection: 'row',
     backgroundColor: '#add8e6',
     paddingHorizontal: 16,
     paddingVertical: 8,
     borderRadius: 56,
     alignItems: 'center',
+    justifyContent: 'center',
   },
   text: {
     color: '#fff',
