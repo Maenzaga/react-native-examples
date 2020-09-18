d114fe67bd66d61b66689c73bce2d2306244497b
Roberto Tatasciore <tatasciorecont@gmail.com>
2020-09-18 18:38:08 +0200

    Validate username

--- src/modules/signup/screens/RegistrationScreen.tsx
+++ src/modules/signup/screens/RegistrationScreen.tsx
@@ -1,6 +1,14 @@
 import React, {useState} from 'react';
-import {ScrollView, Button, View, TextInput, AsyncStorage} from 'react-native';
+import {
+  ScrollView,
+  Button,
+  View,
+  TextInput,
+  AsyncStorage,
+  Alert,
+} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
+import {validateUsername} from 'src/utils/validators';
 
 export const RegistrationScreen = () => {
   const [username, setUsername] = useState('');
@@ -13,6 +21,27 @@ export const RegistrationScreen = () => {
 
   const onPasswordChanged = (password: string) => {
     setPassword(password);
+  };
+
+  const onRegisterPress = () => {
+    if (!username.length || !password.length) {
+      Alert.alert('Error', 'Please fill in both fields');
+    } else if (!validateUsername(username)) {
+      Alert.alert('Error', 'Invalid username');
+    } else {
+      AsyncStorage.getItem('registered_users').then((json: string | null) => {
+        const registeredUsers: string[] = json ? JSON.parse(json) : [];
+        console.log('Registerd users before', registeredUsers);
+        registeredUsers.push(username);
+        console.log('Registerd users after', registeredUsers);
+        AsyncStorage.setItem(
+          'registered_users',
+          JSON.stringify(registeredUsers),
+        ).then(() => {
+          navigation.goBack();
+        });
+      });
+    }
   };
 
   return (
@@ -29,25 +58,7 @@ export const RegistrationScreen = () => {
         onChangeText={onPasswordChanged}
       />
       <View style={{marginVertical: 4}} />
-      <Button
-        title="Sign up"
-        onPress={() => {
-          AsyncStorage.getItem('registered_users').then(
-            (json: string | null) => {
-              const registeredUsers: string[] = json ? JSON.parse(json) : [];
-              console.log('Registerd users before', registeredUsers);
-              registeredUsers.push(username);
-              console.log('Registerd users after', registeredUsers);
-              AsyncStorage.setItem(
-                'registered_users',
-                JSON.stringify(registeredUsers),
-              ).then(() => {
-                navigation.goBack();
-              });
-            },
-          );
-        }}
-      />
+      <Button title="Sign up" onPress={onRegisterPress} />
     </ScrollView>
   );
 };
